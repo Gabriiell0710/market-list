b23ee8a8d752ce96bfd1c2f6c16f4e8a32ff8f67
Gabriel Carvalho <gabriiell0710@gmail.com>
2024-02-09 17:35:32 -0300

    refactor index, style, script

--- script.js
+++ script.js
@@ -13,66 +13,73 @@ const pet = document.querySelector("#pet");
 
 const modal_exit = document.querySelector("#modal_exit");
 
+
+function open_modal (){
+    document.querySelector("#modal").style.display = 'flex';
+}
+
 modal_exit.addEventListener('click', () => {
     document.querySelector("#modal").style.display = 'none';
 })
 
 grocery.addEventListener('click', () => {
-    document.querySelector("#modal").style.display = 'flex';
+    open_modal();
     document.querySelector("#modal-title").textContent = "Mercearia";
 })
 
 canned.addEventListener('click', () => {
-    document.querySelector("#modal").style.display = 'flex';
+    open_modal();
     document.querySelector("#modal-title").textContent = "Enlatados";
 })
 
 bakery.addEventListener('click', () => {
-    document.querySelector("#modal").style.display = 'flex';
+    open_modal();
     document.querySelector("#modal-title").textContent = "Padaria";
 })
 
 drinks.addEventListener('click', () => {
-    document.querySelector("#modal").style.display = 'flex';
+    open_modal();
     document.querySelector("#modal-title").textContent = "Bebidas";
 })
 
 meat.addEventListener('click', () => {
-    document.querySelector("#modal").style.display = 'flex';
+    open_modal();
     document.querySelector("#modal-title").textContent = "Carnes";
 })
 
 cold.addEventListener('click', () => {
-    document.querySelector("#modal").style.display = 'flex';
+    open_modal();
     document.querySelector("#modal-title").textContent = "Frios";
 })
 
 fruits.addEventListener('click', () => {
-    document.querySelector("#modal").style.display = 'flex';
+    open_modal();
     document.querySelector("#modal-title").textContent = "Frutas";
 })
 
 vegetables.addEventListener('click', () => {
-    document.querySelector("#modal").style.display = 'flex';
+    open_modal();
     document.querySelector("#modal-title").textContent = "Legumes";
 })
 
 cleaning.addEventListener('click', () => {
-    document.querySelector("#modal").style.display = 'flex';
+    open_modal();
     document.querySelector("#modal-title").textContent = "Limpeza";
 })
 
 utilities.addEventListener('click', () => {
-    document.querySelector("#modal").style.display = 'flex';
+    open_modal();
     document.querySelector("#modal-title").textContent = "Utilidades";
 })
 
 hygiene.addEventListener('click', () => {
-    document.querySelector("#modal").style.display = 'flex';
+    open_modal();
     document.querySelector("#modal-title").textContent = "Higiene";
 })
 
 pet.addEventListener('click', () => {
-    document.querySelector("#modal").style.display = 'flex';
+    open_modal();
     document.querySelector("#modal-title").textContent = "Pet";
 })
+
+

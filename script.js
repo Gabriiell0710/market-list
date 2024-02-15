fee493905db2cf53b3d688b208cc37dda4b58490
Gabriel Carvalho <gabriiell0710@gmail.com>
2024-02-15 18:55:05 -0300

    refactor index and style

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
-const grocery = document.querySelector("#grocery");
-const canned = document.querySelector("#canned");
-const bakery = document.querySelector("#bakery");
-const drinks = document.querySelector("#drinks");
-const meat = document.querySelector("#meat");
-const cold = document.querySelector("#cold");
-const fruits = document.querySelector("#fruits");
-const vegetables = document.querySelector("#vegetables");
-const cleaning = document.querySelector("#cleaning");
-const utilities = document.querySelector("#utilities");
-const hygiene = document.querySelector("#hygiene");
-const pet = document.querySelector("#pet");
-
-const modal_exit = document.querySelector("#modal_exit");
+function open_modal (){
+    document.querySelector("#modal").style.display = 'flex';
+}
 
 
-async function show_products(idCategory){
+let buttons = document.querySelectorAll('[id^="btn_"]');
+buttons.forEach(button => {
+    button.addEventListener('click', (evt) => {
+        open_modal();
+        let index = button.id.substring(4);
+        load_products(index);
+        document.querySelector("#modal-title").textContent = button.textContent;
+    })
+    const modal_exit = document.querySelector("#modal_exit").addEventListener('click', () => {
+        document.querySelector("#modal").style.display = 'none';
+    })
+})
+
+
+async function load_products(idCategory){
 
     try {
         const response = await fetch('http://localhost:8080/products/category/' + idCategory)
-    
         const data = await response.json();
     
         const productList = document.querySelector("#product-list");
         productList.innerHTML = "";
         const arrayLength = data.length;
+
+        const productQtdInput = document.querySelector(".modal-inputs");
+        productQtdInput.innerHTML = '';
     
         for(var i = 0; i < arrayLength - 1; i++){
             const listItem = document.createElement("li");
             listItem.textContent = data[i].name;
             productList.appendChild(listItem);
+
+            const inputQtd = document.createElement('input')
+            inputQtd.type = "number";
+            inputQtd.min = 0;
+            productQtdInput.appendChild(inputQtd);
         }
-        
-        console.log(data);
     
     } catch (error) {
         console.log("error")
@@ -40,92 +48,11 @@ async function show_products(idCategory){
 }
 
 
-grocery.addEventListener('click', (event) => {
-    event.preventDefault();
-    open_modal();
-    document.querySelector("#modal-title").textContent = "Mercearia";
-    show_products(1);
-    
-    
-})
-
-canned.addEventListener('click', () => {
-    open_modal();
-    document.querySelector("#modal-title").textContent = "Enlatados";
-    show_products(2);
-})
-
-bakery.addEventListener('click', () => {
-    open_modal();
-    document.querySelector("#modal-title").textContent = "Padaria";
-    show_products(3);
-})
-
-drinks.addEventListener('click', () => {
-    open_modal();
-    document.querySelector("#modal-title").textContent = "Bebidas";
-    show_products(4);
-})
-
-meat.addEventListener('click', () => {
-    open_modal();
-    document.querySelector("#modal-title").textContent = "Carnes";
-    show_products(5);
-})
-
-cold.addEventListener('click', () => {
-    open_modal();
-    document.querySelector("#modal-title").textContent = "Frios";
-    show_products(6);
-})
-
-fruits.addEventListener('click', () => {
-    open_modal();
-    document.querySelector("#modal-title").textContent = "Frutas";
-    show_products(7);
-})
-
-vegetables.addEventListener('click', () => {
-    open_modal();
-    document.querySelector("#modal-title").textContent = "Legumes";
-    show_products(8);
-})
-
-cleaning.addEventListener('click', () => {
-    open_modal();
-    document.querySelector("#modal-title").textContent = "Limpeza";
-    show_products(9);
-})
-
-utilities.addEventListener('click', () => {
-    open_modal();
-    document.querySelector("#modal-title").textContent = "Utilidades";
-    show_products(10);
-})
-
-hygiene.addEventListener('click', () => {
-    open_modal();
-    document.querySelector("#modal-title").textContent = "Higiene";show_products(11);
-})
-
-pet.addEventListener('click', () => {
-    open_modal();
-    document.querySelector("#modal-title").textContent = "Pet";
-    show_products(12);
-})
 
 
 
 
 
-function open_modal (){
-    document.querySelector("#modal").style.display = 'flex';
-}
 
-
-
-modal_exit.addEventListener('click', () => {
-    document.querySelector("#modal").style.display = 'none';
-})
 
 

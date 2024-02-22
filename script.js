1b706471d884e57841a3b0d3d76e5328a5426b9e
Gabriel Carvalho <gabriiell0710@gmail.com>
2024-02-22 19:32:20 -0300

    feat object

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ let buttons = document.querySelectorAll('[id^="btn_"]');
 buttons.forEach(button => {
     button.addEventListener('click', (evt) => {
         open_modal();
-        let index = button.id.substring(4);
-        load_products(index);
+        let idCategory = button.id.substring(4);
+        load_products(idCategory);
         document.querySelector("#modal-title").textContent = button.textContent;
+        showIndexButton(idCategory);
     })
     const modal_exit = document.querySelector("#modal_exit").addEventListener('click', () => {
         document.querySelector("#modal").style.display = 'none';
@@ -38,6 +39,7 @@ async function load_products(idCategory){
             const inputQtd = document.createElement('input')
             inputQtd.type = "number";
             inputQtd.min = 0;
+            inputQtd.classList.add('modal-input');
             productQtdInput.appendChild(inputQtd);
         }
     
@@ -45,6 +47,52 @@ async function load_products(idCategory){
         console.log("error")
     }
 
+}
+
+
+const button = document.querySelector("#modal-btn");
+button.addEventListener('click', () => {
+    var confirmation = confirm("Deseja salvar os itens?")
+    if(confirmation){
+        addList();
+        document.querySelector("#modal").style.display = 'none';
+        console.log(catID);
+    }
+    
+    
+})
+
+function getProducts(){
+    var product_item = document.querySelectorAll("li");
+    var productArray = [];
+    product_item.forEach(elem => {
+        productArray.push(elem.textContent);
+    });
+    return productArray;
+}
+
+function getQtd(){
+    var item_qtd = document.querySelectorAll(".modal-input");
+    var qndArray = [];
+    item_qtd.forEach(elem => {
+        qndArray.push(elem.value);
+    });
+    return qndArray;
+}
+
+
+function addList(){
+    var itens_list = [];
+    itens_list.push({name: getProducts(), qtd: getQtd()});
+    console.log(itens_list); 
+}
+
+
+var catID = '';
+
+function showIndexButton(idCategory){
+    catID = idCategory;
+    
 }
 
 
